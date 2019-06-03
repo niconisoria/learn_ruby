4db22df86d824080da25a1e91d3625f2cf624402
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-06-03 00:39:47 -0300

    Solved 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0  
+  end
+
+  def time_string
+    @hours = seconds / 3600
+    @minutes = (seconds % 3600) / 60
+    @seconds = (seconds % 3600) % 60
+    padded(@hours) + ":" + padded(@minutes) + ":" + padded(@seconds)
+  end
+
+  def padded number
+    number < 10 ? "0" + number.to_s : number.to_s
+  end
+
 end
