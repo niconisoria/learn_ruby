247ad29618562c52924114217dd3ae84db2762d1
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-06-03 00:39:19 -0300

    Solved 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,11 @@
 class Book
-# write your code here
+    def title
+        @title.split.each_with_index.map do |word, i|
+            %w(and the of in a an).include?(word) && i != 0 ? word : word.capitalize
+        end.join(" ")
+    end
+
+    def title=(str)
+        @title = str
+    end
 end
