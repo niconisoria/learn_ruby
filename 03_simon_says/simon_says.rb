08bb52260b52c70b2f0c4be3a2783180f6b9d2db
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-06-03 00:38:30 -0300

    Solved 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,25 @@
-#write your code here
+def echo word
+    word
+end
+
+def shout word
+    word.upcase
+end
+
+def repeat word, number=2
+    ((word+" ")*number).strip
+end
+
+def start_of_word word, number
+    word[0,number]
+end
+
+def first_word phrase
+    phrase.split[0]
+end
+
+def titleize phrase
+    words = phrase.split.map(&lambda{|word| %w(the and over).include?(word) ? word : word.capitalize})
+    words.first.capitalize!
+    words.join(" ")
+end
