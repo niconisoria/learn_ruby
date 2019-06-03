f446e65e35324fb6ccaa23f62675a71ddb27e354
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-06-03 00:38:49 -0300

    Solved 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
-#write your code here
+def translate phrase
+    phrase.split.map(&lambda { |word|
+        aux = []
+        word.chars.each_with_index do |c, i|
+            if (%w(a e i o u).include?(c) && aux[i-1] != 'q')
+                break
+            else
+                aux[i] = c
+                word[i] = " "
+            end
+        end
+        word = word.strip + aux.join("") + "ay"
+    }).join(" ")
+end
