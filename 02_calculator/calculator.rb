5aabf489b070a9a487493970a803c19987a7c99a
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-06-03 00:38:04 -0300

    Solved 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add first, second
+    first + second
+end
+
+def subtract first, second
+    first - second
+end
+
+def sum array
+    sum = 0
+    array.each do |e| 
+        sum += e
+    end
+    sum
+end
+
+def multiply *args
+    sum = 1
+    args.each do |e|
+        sum *= e
+    end
+    sum
+end
+
+def power first, second
+    first ** second
+end
+
+def factorial number
+    number == 0 ? 1 : number * factorial(number-1)
+end
